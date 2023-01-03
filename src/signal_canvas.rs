4709bdc3c0a1db1b5e35dff6bb682ca7823c6f5c
TheZoq2 <frans.skarman@protonmail.com>
2023-01-03 23:16:24 +0100

    Segfault in rust :eyes

--- src/signal_canvas.rs
+++ src/signal_canvas.rs
@@ -1,6 +1,9 @@
 use std::collections::BTreeMap;
 
-use fastwave_backend::{Signal, SignalValue, SignalIdx};
+use eframe::egui::{self, Sense, Painter};
+use eframe::emath;
+use eframe::epaint::{Color32, Pos2, Rect, Rounding, Stroke, PathShape};
+use fastwave_backend::{Signal, SignalIdx, SignalValue};
 use num::{BigInt, FromPrimitive};
 use num::{BigRational, BigUint};
 
@@ -8,27 +11,15 @@ use crate::viewport::Viewport;
 use crate::{Message, State};
 
 impl State {
-    fn draw(
-        &self,
-        _interaction: &Interaction,
-        _theme: &Theme,
-        bounds: Rectangle,
-        _cursor: Cursor,
-    ) -> Vec<Geometry> {
-        let mut frame = Frame::new(bounds.size());
-        let background = Path::rectangle(Point::ORIGIN, frame.size());
-        frame.fill(&background, Color::from_rgb8(0, 0, 0));
+    pub fn draw_signals(&self, ui: &mut egui::Ui) {
+        let (response, mut painter) = ui.allocate_painter(ui.available_size(), Sense::hover());
 
-        frame.scale(1.);
-        frame.translate(Vector::new(0., 0.));
+        let to_screen = emath::RectTransform::from_to(
+            Rect::from_min_size(Pos2::ZERO, response.rect.size()),
+            response.rect,
+        );
 
-        for x in 0..100 {
-            frame.fill_rectangle(
-                Point::new(x as f32 * 10., 0.),
-                Size::new(1., frame.height()),
-                Color::from_rgb8(10, 10, 10),
-            );
-        }
+        painter.rect_filled(response.rect, Rounding::none(), Color32::from_rgb(0, 0, 0));
 
         let mut prev_values = BTreeMap::new();
 
@@ -40,10 +31,10 @@ impl State {
         let max_time = BigRational::from_integer(self.num_timestamps.clone());
 
         if let Some(vcd) = &self.vcd {
-            let frame_width = frame.width();
+            let frame_width = response.rect.width();
             'outer: for x in 0..frame_width as u32 {
-                let time = self
-                    .viewport_to_time(BigRational::from_float(x as f64).unwrap(), frame.width());
+                let time =
+                    self.viewport_to_time(BigRational::from_float(x as f64).unwrap(), frame_width);
                 if time < BigRational::from_float(0.).unwrap() {
                     continue;
                 }
@@ -60,7 +51,7 @@ impl State {
                         let prev = prev_values.get(idx);
                         if let Some((old_x, old_val)) = prev_values.get(idx) {
                             self.draw_signal(
-                                &mut frame,
+                                &mut painter,
                                 y as f32,
                                 idx,
                                 &sig,
@@ -77,25 +68,23 @@ impl State {
                         if prev.map(|(_, v)| v) != Some(&val) {
                             prev_values.insert(*idx, (x, val));
                         }
-
-                        // If this was the last x in the vcd file, we are done
-                        // drawing, so we can reak out of the outer loop
                     }
                 }
+
+                // If this was the last x in the vcd file, we are done
+                // drawing, so we can reak out of the outer loop
                 if is_last_x {
                     break 'outer;
                 }
             }
         }
-
-        vec![frame.into_geometry()]
     }
 }
 
 impl State {
     fn draw_signal(
         &self,
-        frame: &mut Frame,
+        painter: &mut Painter,
         y: f32,
         signal_idx: &SignalIdx,
         signal: &Signal,
@@ -106,56 +95,67 @@ impl State {
     ) {
         let y_start = y as f32 * (cfg.line_height + cfg.padding);
         let abs_point =
-            |x: f32, rel_y: f32| Point::new(x as f32, y_start + (1. - rel_y) * cfg.line_height);
+            |x: f32, rel_y: f32| Pos2::new(x as f32, y_start + (1. - rel_y) * cfg.line_height);
 
         if old_val != val || force_redraw {
             if signal.num_bits() == Some(1) {
                 let (old_height, old_color) = old_val.bool_drawing_spec();
                 let (new_height, _) = val.bool_drawing_spec();
 
-                let mut path = PathBuilder::new();
-                path.line_to(abs_point(old_x as f32, old_height));
-                path.line_to(abs_point(x as f32, old_height));
-                path.line_to(abs_point(x as f32, new_height));
-                frame.stroke(
-                    &path.build(),
-                    Stroke::default().with_color(old_color).with_width(1.0),
-                );
+                let stroke = Stroke {
+                    color: old_color,
+                    width: 1.,
+                    .. Default::default()
+                };
 
+                painter.add(PathShape::line(
+                    vec! [
+                        abs_point(old_x as f32, old_height),
+                        abs_point(x as f32, old_height),
+                        abs_point(x as f32, new_height)
+                    ],
+                    stroke
+                ));
             } else {
-                let mut border = PathBuilder::new();
-
-                let transition_width = (x - old_x).min(6) as f32;
-                border.line_to(abs_point(old_x as f32, 0.5));
-                border.line_to(abs_point(old_x as f32 + transition_width / 2., 1.0));
-                border.line_to(abs_point(x as f32 - transition_width/2., 1.0));
-                border.line_to(abs_point(x as f32, 0.5));
-                border.line_to(abs_point(x as f32 - transition_width/2., 0.0));
-                border.line_to(abs_point(old_x as f32 + transition_width / 2., 0.0));
-                border.line_to(abs_point(old_x as f32, 0.5));
-
-
-
                 let stroke_color = match old_val.value_kind() {
                     ValueKind::HighImp => style::c_yellow(),
                     ValueKind::Undef => style::c_red(),
                     ValueKind::Normal => style::c_green(),
                 };
 
-                frame.stroke(
-                    &border.build(),
-                    Stroke::default().with_color(stroke_color).with_width(1.0),
-                );
+                let stroke = Stroke {
+                    color: stroke_color,
+                    width: 1.,
+                    .. Default::default()
+                };
 
+                let transition_width = (x - old_x).min(6) as f32;
+
+                painter.add(PathShape::line(
+                    vec! [
+                        abs_point(old_x as f32, 0.5),
+                        abs_point(old_x as f32 + transition_width / 2., 1.0),
+                        abs_point(x as f32 - transition_width / 2., 1.0),
+                        abs_point(x as f32, 0.5),
+                        abs_point(x as f32 - transition_width / 2., 0.0),
+                        abs_point(old_x as f32 + transition_width / 2., 0.0),
+                        abs_point(old_x as f32, 0.5),
+                    ],
+                    stroke
+                ));
+
+                /*
                 let text_size = cfg.line_height - 4.;
                 let char_width = text_size * 0.53;
 
                 let text_area = (x - old_x) as f32 - transition_width;
                 let num_chars = (text_area / char_width).floor();
-                let fits_text =  num_chars >= 1.;
+                let fits_text = num_chars >= 1.;
 
                 if fits_text {
-                    let translator_name = self.signal_format.get(&signal_idx)
+                    let translator_name = self
+                        .signal_format
+                        .get(&signal_idx)
                         .unwrap_or_else(|| &self.translators.default);
                     let translator = &self.translators.inner[translator_name];
 
@@ -168,8 +168,7 @@ impl State {
                             .take(num_chars as usize - 1)
                             .chain(['…'].into_iter())
                             .collect::<String>()
-                    }
-                    else {
+                    } else {
                         full_text
                     };
 
@@ -180,11 +179,12 @@ impl State {
                         size: text_size,
                         font: self.font,
                         vertical_alignment: iced::alignment::Vertical::Center,
-                        .. Default::default()
+                        ..Default::default()
                     };
 
                     frame.fill_text(text)
                 }
+                */
             }
         }
     }
@@ -202,66 +202,66 @@ impl State {
         time
     }
 
-    pub fn handle_scroll(
-        &self,
-        cursor: Cursor,
-        bounds: Rectangle,
-        delta: ScrollDelta,
-    ) -> Option<Message> {
-        match delta {
-            ScrollDelta::Lines { x: _, y } => {
-                // Zoom or scroll
-                if self.control_key {
-                    if let Some(cursor_pos) = cursor.position_in(&bounds) {
-                        let Viewport {
-                            curr_left: left,
-                            curr_right: right,
-                            ..
-                        } = &self.viewport;
+    // pub fn handle_scroll(
+    //     &self,
+    //     cursor: Cursor,
+    //     bounds: Rectangle,
+    //     delta: ScrollDelta,
+    // ) -> Option<Message> {
+    //     match delta {
+    //         ScrollDelta::Lines { x: _, y } => {
+    //             // Zoom or scroll
+    //             if self.control_key {
+    //                 if let Some(cursor_pos) = cursor.position_in(&bounds) {
+    //                     let Viewport {
+    //                         curr_left: left,
+    //                         curr_right: right,
+    //                         ..
+    //                     } = &self.viewport;
 
-                        let cursor_pos = self.viewport_to_time(
-                            BigRational::from_float(cursor_pos.x).unwrap(),
-                            bounds.width,
-                        );
+    //                     let cursor_pos = self.viewport_to_time(
+    //                         BigRational::from_float(cursor_pos.x).unwrap(),
+    //                         bounds.width,
+    //                     );
 
-                        // - to get zoom in the natural direction
-                        let scale = BigRational::from_float(1. - y / 10.).unwrap();
+    //                     // - to get zoom in the natural direction
+    //                     let scale = BigRational::from_float(1. - y / 10.).unwrap();
 
-                        let target_left = (left - &cursor_pos) * &scale + &cursor_pos;
-                        let target_right = (right - &cursor_pos) * &scale + &cursor_pos;
+    //                     let target_left = (left - &cursor_pos) * &scale + &cursor_pos;
+    //                     let target_right = (right - &cursor_pos) * &scale + &cursor_pos;
 
-                        // TODO: Do not just round here, this will not work
-                        // for small zoom levels
-                        Some(Message::ChangeViewport(Viewport {
-                            curr_left: target_left.clone().round(),
-                            curr_right: target_right.clone().round(),
-                        }))
-                    } else {
-                        None
-                    }
-                } else {
-                    // Scroll 5% of the viewport per scroll event
-                    let scroll_step = (&self.viewport.curr_right - &self.viewport.curr_left)
-                        / BigInt::from_u32(20).unwrap();
+    //                     // TODO: Do not just round here, this will not work
+    //                     // for small zoom levels
+    //                     Some(Message::ChangeViewport(Viewport {
+    //                         curr_left: target_left.clone().round(),
+    //                         curr_right: target_right.clone().round(),
+    //                     }))
+    //                 } else {
+    //                     None
+    //                 }
+    //             } else {
+    //                 // Scroll 5% of the viewport per scroll event
+    //                 let scroll_step = (&self.viewport.curr_right - &self.viewport.curr_left)
+    //                     / BigInt::from_u32(20).unwrap();
 
-                    let to_scroll = BigRational::from(scroll_step.clone())
-                        * BigRational::from_float(y).unwrap();
+    //                 let to_scroll = BigRational::from(scroll_step.clone())
+    //                     * BigRational::from_float(y).unwrap();
 
-                    let target_left = &self.viewport.curr_left + &to_scroll;
-                    let target_right = &self.viewport.curr_right + &to_scroll;
-                    Some(Message::ChangeViewport(Viewport {
-                        curr_left: target_left.clone(),
-                        curr_right: target_right.clone(),
-                    }))
-                }
-            }
-            ScrollDelta::Pixels { .. } => {
-                // TODO
-                println!("NOTE: Pixel scroll is unimplemented");
-                None
-            }
-        }
-    }
+    //                 let target_left = &self.viewport.curr_left + &to_scroll;
+    //                 let target_right = &self.viewport.curr_right + &to_scroll;
+    //                 Some(Message::ChangeViewport(Viewport {
+    //                     curr_left: target_left.clone(),
+    //                     curr_right: target_right.clone(),
+    //                 }))
+    //             }
+    //         }
+    //         ScrollDelta::Pixels { .. } => {
+    //             // TODO
+    //             println!("NOTE: Pixel scroll is unimplemented");
+    //             None
+    //         }
+    //     }
+    // }
 }
 
 struct DrawConfig {
@@ -277,7 +277,7 @@ enum ValueKind {
 
 trait SignalExt {
     fn value_kind(&self) -> ValueKind;
-    fn bool_drawing_spec(&self) -> (f32, Color);
+    fn bool_drawing_spec(&self) -> (f32, Color32);
 }
 
 impl SignalExt for SignalValue {
@@ -298,7 +298,7 @@ impl SignalExt for SignalValue {
     }
 
     /// Return the height and color with which to draw this value if it is a boolean
-    fn bool_drawing_spec(&self) -> (f32, Color) {
+    fn bool_drawing_spec(&self) -> (f32, Color32) {
         match (self.value_kind(), self) {
             (ValueKind::HighImp, _) => (0.5, style::c_yellow()),
             (ValueKind::Undef, _) => (0.5, style::c_red()),
@@ -316,31 +316,33 @@ impl SignalExt for SignalValue {
     }
 }
 
+
+
 mod style {
-    use iced::Color;
+    use eframe::epaint::Color32;
 
-    fn c_min() -> f32 {
-        0.3
+    fn c_min() -> u8 {
+        64
     }
-    fn c_max() -> f32 {
-        1.0
+    fn c_max() -> u8 {
+        255
     }
-    fn c_mid() -> f32 {
-        0.5
-    }
-
-    pub fn c_green() -> Color {
-        Color::from_rgb(c_min(), c_max(), c_min())
-    }
-    pub fn c_dark_green() -> Color {
-        Color::from_rgb(c_min(), c_mid(), c_min())
+    fn c_mid() -> u8 {
+        128
     }
 
-    pub fn c_red() -> Color {
-        Color::from_rgb(c_max(), c_min(), c_min())
+    pub fn c_green() -> Color32 {
+        Color32::from_rgb(c_min(), c_max(), c_min())
+    }
+    pub fn c_dark_green() -> Color32 {
+        Color32::from_rgb(c_min(), c_mid(), c_min())
     }
 
-    pub fn c_yellow() -> Color {
-        Color::from_rgb(c_max(), c_max(), c_min())
+    pub fn c_red() -> Color32 {
+        Color32::from_rgb(c_max(), c_min(), c_min())
+    }
+
+    pub fn c_yellow() -> Color32 {
+        Color32::from_rgb(c_max(), c_max(), c_min())
     }
 }
