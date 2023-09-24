ad8387118dabf36a0d384ffbdb0478acdafc3f1a
Lucas Klemmer <lucas.klemmer@jku.at>
2023-09-24 23:05:42 +0200

    allow global signal names

--- src/commands.rs
+++ src/commands.rs
@@ -1,8 +1,8 @@
-use std::{collections::BTreeMap, fs};
+use std::{collections::BTreeMap, fs, str::FromStr};
 
 use crate::{
     util::{alpha_idx_to_uint_idx, uint_idx_to_alpha_idx},
-    Message, State,
+    Message, SignalNameType, State,
 };
 
 use fzcmd::{expand_command, Command, FuzzyOutput, ParamGreed};
@@ -116,6 +116,7 @@ pub fn get_parser(state: &State) -> Command<Message> {
             "load_url",
             "set_signal_color",
             "reload_config",
+            "signal_name_type_set",
         ]
         .into_iter()
         .map(|s| s.into())
@@ -188,6 +189,18 @@ pub fn get_parser(state: &State) -> Command<Message> {
                     }),
                 ),
                 "reload_config" => Some(Command::Terminal(Message::ReloadConfig)),
+                "signal_name_type_set" => single_word(
+                    vec![
+                        "Local".to_string(),
+                        "Unique".to_string(),
+                        "Global".to_string(),
+                    ],
+                    Box::new(|word| {
+                        Some(Command::Terminal(Message::ChangeSignalNameType(
+                            SignalNameType::from_str(word).unwrap_or(SignalNameType::Local),
+                        )))
+                    }),
+                ),
                 _ => None,
             }
         }),
