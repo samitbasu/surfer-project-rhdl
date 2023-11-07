16a5bbd34a28bd2a9a4eb56ce295cd094466fa3f
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2023-11-07 13:17:34 +0100

    Fix time string formatting

--- src/time.rs
+++ src/time.rs
@@ -23,21 +23,31 @@ pub fn timescale_menu(ui: &mut egui::Ui, msgs: &mut Vec<Message>, wanted_timesca
     }
 }
 
+fn strip_trailing_zeros_and_period(time: String) -> String {
+    if time.contains('.') {
+        time.trim_end_matches('0').trim_end_matches('.').to_string()
+    } else {
+        time
+    }
+}
+
 pub fn time_string(time: &BigInt, metadata: &Metadata, wanted_timescale: &Timescale) -> String {
     let wanted_exponent = timescale_to_exponent(wanted_timescale);
     let data_exponent = timescale_to_exponent(&metadata.timescale.1);
     let exponent_diff = wanted_exponent - data_exponent;
     if exponent_diff >= 0 {
         let precision = exponent_diff as usize;
-        format!(
-            "{scaledtime:.precision$} {wanted_timescale}",
+        let scaledtime = strip_trailing_zeros_and_period(format!(
+            "{scaledtime:.precision$}",
             scaledtime = BigRational::new(
                 time * metadata.timescale.0.unwrap_or(1),
                 (BigInt::from(10)).pow(exponent_diff as u32)
             )
             .to_f64()
             .unwrap_or(f64::NAN)
-        )
+        ));
+
+        format!("{scaledtime} {wanted_timescale}")
     } else {
         format!(
             "{scaledtime} {wanted_timescale}",
