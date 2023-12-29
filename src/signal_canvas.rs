9939b8074a8a3962b807d45107f905ac6a9f5a45
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2023-12-29 12:33:41 +0100

    Add configurability for signal tooltip

--- src/signal_canvas.rs
+++ src/signal_canvas.rs
@@ -416,11 +416,7 @@ impl State {
                 [first, second, ..] => second - first > 15.,
             };
             let ticks = &draw_data.ticks;
-            if !ticks.is_empty()
-                && self
-                    .show_ticks
-                    .unwrap_or_else(|| self.config.layout.show_ticks())
-            {
+            if !ticks.is_empty() && self.show_ticks.unwrap_or(self.config.layout.show_ticks()) {
                 let stroke = Stroke {
                     color: self.config.ticks.style.color,
                     width: self.config.ticks.style.width,
