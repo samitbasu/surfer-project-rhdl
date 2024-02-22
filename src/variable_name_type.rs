bcc346abffdb7e579ef6184af0c6362b0cbf6db9
Lucas Klemmer <lucas.klemmer@jku.at>
2024-02-22 15:56:56 +0100

    add DisplayedItemRef

--- src/variable_name_type.rs
+++ src/variable_name_type.rs
@@ -42,75 +42,83 @@ impl std::fmt::Display for VariableNameType {
 impl WaveData {
     pub fn compute_variable_display_names(&mut self) {
         let full_names = self
-            .displayed_items
+            .displayed_items_order
             .iter()
+            .map(|id| self.displayed_items.get(id))
             .filter_map(|item| match item {
-                DisplayedItem::Variable(variable) => Some(variable.variable_ref.clone()),
+                Some(DisplayedItem::Variable(variable)) => Some(variable.variable_ref.clone()),
                 _ => None,
             })
             .unique()
             .collect_vec();
 
-        for item in &mut self.displayed_items {
-            match item {
-                DisplayedItem::Variable(variable) => {
-                    let local_name = variable.variable_ref.name.clone();
-                    variable.display_name = match variable.display_name_type {
-                        VariableNameType::Local => local_name,
-                        VariableNameType::Global => variable.variable_ref.full_path_string(),
-                        VariableNameType::Unique => {
-                            /// This function takes a full variable name and a list of other
-                            /// full variable names and returns a minimal unique variable name.
-                            /// It takes scopes from the back of the variable until the name is unique.
-                            fn unique(variable: &VariableRef, variables: &[VariableRef]) -> String {
-                                let other_variables = variables
-                                    .iter()
-                                    .filter(|&s| {
-                                        *s.full_path_string() != variable.full_path_string()
-                                    })
-                                    .collect_vec();
+        for id in &self.displayed_items_order {
+            self.displayed_items
+                .entry(id.clone())
+                .and_modify(|item| match item {
+                    DisplayedItem::Variable(variable) => {
+                        let local_name = variable.variable_ref.name.clone();
+                        variable.display_name = match variable.display_name_type {
+                            VariableNameType::Local => local_name,
+                            VariableNameType::Global => variable.variable_ref.full_path_string(),
+                            VariableNameType::Unique => {
+                                /// This function takes a full variable name and a list of other
+                                /// full variable names and returns a minimal unique variable name.
+                                /// It takes scopes from the back of the variable until the name is unique.
+                                fn unique(
+                                    variable: &VariableRef,
+                                    variables: &[VariableRef],
+                                ) -> String {
+                                    let other_variables = variables
+                                        .iter()
+                                        .filter(|&s| {
+                                            *s.full_path_string() != variable.full_path_string()
+                                        })
+                                        .collect_vec();
 
-                                fn take_front(v: &VariableRef, l: usize) -> String {
-                                    if l == 0 {
-                                        v.name.clone()
-                                    } else {
-                                        format!(
-                                            "{}{}.{}",
-                                            if l < v.path.strs().len() { "…" } else { "" },
-                                            v.path.strs().iter().rev().take(l).rev().join("."),
-                                            v.name
-                                        )
+                                    fn take_front(v: &VariableRef, l: usize) -> String {
+                                        if l == 0 {
+                                            v.name.clone()
+                                        } else {
+                                            format!(
+                                                "{}{}.{}",
+                                                if l < v.path.strs().len() { "…" } else { "" },
+                                                v.path.strs().iter().rev().take(l).rev().join("."),
+                                                v.name
+                                            )
+                                        }
                                     }
+
+                                    let mut l = 0;
+                                    while other_variables
+                                        .iter()
+                                        .map(|v| take_front(v, l))
+                                        .contains(&take_front(variable, l))
+                                    {
+                                        l += 1;
+                                    }
+                                    take_front(variable, l)
                                 }
 
-                                let mut l = 0;
-                                while other_variables
-                                    .iter()
-                                    .map(|v| take_front(v, l))
-                                    .contains(&take_front(variable, l))
-                                {
-                                    l += 1;
-                                }
-                                take_front(variable, l)
+                                unique(&variable.variable_ref, &full_names)
                             }
-
-                            unique(&variable.variable_ref, &full_names)
-                        }
-                    };
-                }
-                DisplayedItem::Divider(_) => {}
-                DisplayedItem::Marker(_) => {}
-                DisplayedItem::TimeLine(_) => {}
-                DisplayedItem::Placeholder(_) => {}
-            }
+                        };
+                    }
+                    DisplayedItem::Divider(_) => {}
+                    DisplayedItem::Marker(_) => {}
+                    DisplayedItem::TimeLine(_) => {}
+                    DisplayedItem::Placeholder(_) => {}
+                });
         }
     }
 
     pub fn force_variable_name_type(&mut self, name_type: VariableNameType) {
-        for variable in &mut self.displayed_items {
-            if let DisplayedItem::Variable(variable) = variable {
-                variable.display_name_type = name_type;
-            }
+        for id in &self.displayed_items_order {
+            self.displayed_items.entry(id.clone()).and_modify(|item| {
+                if let DisplayedItem::Variable(variable) = item {
+                    variable.display_name_type = name_type;
+                }
+            });
         }
         self.default_variable_name_type = name_type;
         self.compute_variable_display_names();
