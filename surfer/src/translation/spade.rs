ed97cc489119ae3320a86556465f1b875ca1328a
Gustav Sörnäs <gustav@sornas.net>
2024-07-02 13:11:51 +0200

    use ecolor for custom value color

--- surfer/src/translation/spade.rs
+++ surfer/src/translation/spade.rs
@@ -351,9 +351,7 @@ fn translate_concrete(
 
                             let result = if i == tag {
                                 if name == "None" {
-                                    kind = ValueKind::Custom(
-                                        Color32::DARK_GRAY.to_srgba_unmultiplied(),
-                                    )
+                                    kind = ValueKind::Custom(Color32::DARK_GRAY)
                                 }
 
                                 SubFieldTranslationResult {
@@ -413,7 +411,7 @@ fn translate_concrete(
         }
         ConcreteType::Backward(_) => TranslationResult {
             val: ValueRepr::String("*backward*".to_string()),
-            kind: ValueKind::Custom(Color32::from_gray(128).to_srgba_unmultiplied()),
+            kind: ValueKind::Custom(Color32::from_gray(128)),
             subfields: vec![],
         },
         ConcreteType::Wire(inner) => translate_concrete(val, inner, problematic)?,
