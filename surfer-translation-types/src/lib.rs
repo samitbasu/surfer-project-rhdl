0a16164f9506fe9d02a467e13b81cedbd806c761
Gustav Sörnäs <gustav@sornas.net>
2024-07-16 10:05:39 +0200

    install python3-dev in ci, compile wasm

--- surfer-translation-types/src/lib.rs
+++ surfer-translation-types/src/lib.rs
@@ -1,4 +1,6 @@
 mod field_ref;
+#[cfg(not(target_arch = "wasm32"))]
+pub mod python;
 mod result;
 mod scope_ref;
 mod translator;
@@ -9,7 +11,6 @@ use std::collections::HashMap;
 use derive_more::Display;
 use ecolor::Color32;
 use num::BigUint;
-use pyo3::{pyclass, pymodule};
 
 pub use crate::field_ref::FieldRef;
 pub use crate::result::{
@@ -39,41 +40,6 @@ pub enum ValueKind {
     Warn,
     DontCare,
     Weak,
-}
-
-#[pymodule]
-#[pyo3(name = "surfer")]
-pub fn surfer_pyo3_module(m: &pyo3::Bound<'_, pyo3::types::PyModule>) -> pyo3::PyResult<()> {
-    m.add_class::<PythonValueKind>().unwrap();
-    Ok(())
-}
-
-#[derive(Clone)]
-#[pyclass(name = "ValueKind")]
-pub enum PythonValueKind {
-    Normal {},
-    Undef {},
-    HighImp {},
-    Custom { color: [u8; 4] },
-    Warn {},
-    DontCare {},
-    Weak {},
-}
-
-impl From<PythonValueKind> for ValueKind {
-    fn from(value: PythonValueKind) -> Self {
-        match value {
-            PythonValueKind::Normal {} => ValueKind::Normal,
-            PythonValueKind::Undef {} => ValueKind::Undef,
-            PythonValueKind::HighImp {} => ValueKind::HighImp,
-            PythonValueKind::Custom {
-                color: [r, g, b, a],
-            } => ValueKind::Custom(Color32::from_rgba_unmultiplied(r, g, b, a)),
-            PythonValueKind::Warn {} => ValueKind::Undef,
-            PythonValueKind::DontCare {} => ValueKind::Undef,
-            PythonValueKind::Weak {} => ValueKind::Undef,
-        }
-    }
 }
 
 #[derive(PartialEq)]
